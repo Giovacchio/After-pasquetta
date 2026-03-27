8e55e054d8329e5a50005d090803a64d0ae6727e
Giovacchio <giovacchioxq@gmail.com>
2026-03-27 23:28:21 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'after-pasquetta-v26';
-const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'after-pasquetta-v27';
+const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon192.png', './icon512.png'];
 
 // Ricevi il messaggio dall'app per attivarsi subito
 self.addEventListener('message', e => {
