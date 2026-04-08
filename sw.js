be94681333532c3794fbb3f3fc3822891cb3b84f
Giovacchio <giovacchioxq@gmail.com>
2026-04-08 10:29:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'after-pasquetta-v44';
+const CACHE = 'after-pasquetta-v45';
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // Ricevi il messaggio dall'app per attivarsi subito
